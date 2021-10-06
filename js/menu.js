9f1d44d545f685c418ebd4a646bf705034e5044c
mondejarmarron18 <mondejarmarron18@gmail.com>
2021-10-06 14:50:32 +0800

    Update mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,18 @@
 _ = (el) => document.querySelector(el);
 evt = (el, evt, fn) => el.addEventListener(evt, fn);
 
-evt(_('.menu_btn'), 'click', () => {
-    _('.menu_mobile').classList.toggle('menu_open')
-    _('.menu_btn').classList.toggle('menu_btn_close')
-})
+evt(_(".menu_btn"), "click", () => {
+  _(".menu_mobile").classList.toggle("menu_open");
+  _(".menu_btn").classList.toggle("menu_btn_close");
+});
+
+window.onload = () => {
+  const navLinks = _(".menu_mobile nav").children;
+
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", () => {
+      _(".menu_mobile").classList.toggle("menu_open");
+      _(".menu_btn").classList.toggle("menu_btn_close");
+    });
+  }
+};
